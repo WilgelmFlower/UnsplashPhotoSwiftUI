6b9578b1bd10ccf11614f48eea00c2d28323414d
Mark Wilgelm <markujke@mail.ru>
2023-07-05 10:27:13 +0200

    Initial Commit

---
+++ UnsplashPhotoSwiftUI/UnsplashPhotoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashPhotoSwiftUIApp.swift
+//  UnsplashPhotoSwiftUI
+//
+//  Created by Mark Wilhelm on 05.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashPhotoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
